b1cce13b8d489957ce5f8c56d8cf955dfc7a917e
Suttipong Srikok <fisca2012@gmail.com>
2013-07-28 17:00:08 +0700

    add LuckyNine Project

---
+++ LuckyNine/LuckyNine/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Visible = False
+        Label1.Text = CStr(Int(Rnd() * 10))
+        Label2.Text = CStr(Int(Rnd() * 10))
+        Label3.Text = CStr(Int(Rnd() * 10))
+
+        If (Label1.Text = "9") Or (Label2.Text = "9") Or (Label3.Text = "9") Then
+            PictureBox1.Visible = True
+            Beep()
+        End If
+    End Sub
+End Class
