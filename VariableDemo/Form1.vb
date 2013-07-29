f1ba7ae5fe9295b8d3c3c20fff9879287041910c
Suttipong Srikok <fisca2012@gmail.com>
2013-07-30 01:02:12 +0700

    Create VariableDemo Project

---
+++ VariableDemo/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FirstName As String
+        FirstName = "Suttipong"
+        Label1.Text = FirstName
+        FirstName = "Fisca"
+        Label2.Text = FirstName
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Promt, FullName As String
+        Promt = "กรุณาป้อนชื่อและนามสกุลของคุณ"
+        FullName = InputBox(Promt)
+        Label1.Text = FullName
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Promt, FullName As String
+        Promt = "กรุณาป้อนชื่อและนามสกุลของคุณ"
+        FullName = InputBox(Promt)
+        MsgBox(FullName, , "ชื่อและนามสกุลของคุณ")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+    End Sub
+   
+End Class
